cbb86004edce615ea0ac78553acd0682cfefe990
basakil <basakil@gmail.com>
2022-11-13 16:31:52 +0300

    29: consuming remote modules + refactoring products and cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,11 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import {mount as mount_products} from 'products/ProductsIndex';
+import {mount as mount_cart} from 'cart/CartShow';
+
+
+const productsEl = document.getElementById('my-products');
+mount_products(productsEl);
+
+const cartEl = document.getElementById('my-cart');
+mount_cart(cartEl);
 
 console.log('Container!');
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,24 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// check if in development mode, running products outside of container app.
+if (process.env.NODE_ENV == 'development') {
+  const el = document.getElementById('dev-products');
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
+
